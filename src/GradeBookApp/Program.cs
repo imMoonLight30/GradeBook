2cd98e03e1a05ade76a83a6a6a476c20e4917e53
Chandra <30cpss@gmail.com>
2023-12-10 00:54:08 +0530

    tried some math part in c#

--- src/GradeBookApp/Program.cs
+++ src/GradeBookApp/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        var numbers = new[] {10.2, 10.3 , 10, 15};
+        float sum=0;
+        foreach(float num in numbers){
+            sum += num;
+        }
+        float avg = sum/numbers.Length;
+        Console.Write(avg);
     }
 }
