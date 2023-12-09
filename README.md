830a299a8e17370696e3b742ce63023a75b12aec
Chandra <30cpss@gmail.com>
2023-12-09 23:55:17 +0530

    created new console application

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # GradeBook
 C# Fundamentals
+
+console app - runs on cmd prompt ..
+project unit test - mstest 
+.
+.
+so..on..
---
+++ src/GradeBookApp/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace GradeBookApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
