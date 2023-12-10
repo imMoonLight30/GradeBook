c58d07e37850932ff6d0a17c10362d6df8ada325
Chandra <30cpss@gmail.com>
2023-12-10 14:05:38 +0530

    test project

---
+++ test/GradeBookApp.tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+
+namespace GradeBookApp.tests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        //arrange
+        int num =1;
+        //act
+
+        //assert
+        
+    }
+}
